d5be1463fdc84ac1257265a34953fe597f4f97b4
anushank-lal <anushanklal@gmail.com>
2015-12-24 12:41:32 +0530

    Signed-off-by: anushank-lal <anushanklal@gmail.com>

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -4,14 +4,13 @@ class PaymentController < ApplicationController
   end
 
   def process_payment
-    session[:params] = params
   end
 
   def confirm_payment
     if params[:status]
-      redirect_to session[:params][:payment_return_url]
+      redirect_to params[:payment_return_url]
     else
-      redirect_to session[:params][:payment_return_url]
+      redirect_to params[:payment_return_url]
     end
   end
 
